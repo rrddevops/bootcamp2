54349e4c71ae7f635f02cae3c68aea3731e1e035
Rodrigo Davila <rodrigordavila@gmail.com>
2023-04-07 19:52:51 -0300

    jenkins ec2

---
+++ jenkins-ec2-tf/variables.tf
@@ -0,0 +1,37 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  //default     = "us-east-1"
+}
+
+variable "aws_profile" {
+  description = "AWS profile"
+}
+
+variable "ec2_user_data" {
+  description = "User data script for EC2"
+  type        = string
+  default     = <<EOF
+#!/bin/bash
+sudo yum update -y
+sudo amazon-linux-extras install epel -y
+sudo yum install daemonize -y
+sudo yum install git -y 
+sudo amazon-linux-extras install docker -y 
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade
+sudo amazon-linux-extras install java-openjdk11
+sudo yum install java-1.8.0-openjdk
+java -version
+sudo yum install jenkins -y
+sudo usermod -aG docker jenkins 
+sudo systemctl daemon-reload
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl restart jenkins
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+EOF
+}
