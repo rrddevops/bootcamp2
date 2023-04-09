4346c82ca2d3d00abd5daec22c7f3da83021789f
Rodrigo Davila <rodrigordavila@gmail.com>
2023-04-09 15:42:41 -0300

    versao 13

--- app/index.php
+++ app/index.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <title>
-        AWS Instance - versão 3
+        AWS Instance - versão 13
  </title>
   <style>
         body {
