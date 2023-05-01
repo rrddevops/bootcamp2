3cd2029a356f353bc79c0efc181e2fce4966b675
Rodrigo Davila <rodrigordavila@gmail.com>
2023-05-01 20:53:16 -0300

    teste

--- app/index.php
+++ app/index.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <title>
-        AWS Instance - versão 1
+        AWS Instance - versão 01
  </title>
   <style>
         body {
