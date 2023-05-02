ae73b1742d65da71ab07db52042b2717de8fdf64
Rodrigo Davila <rodrigordavila@gmail.com>
2023-05-01 21:36:52 -0300

    buildversao6

--- app/index.php
+++ app/index.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <title>
-        AWS Instance - versão 04
+        AWS Instance - versão 06
  </title>
   <style>
         body {
